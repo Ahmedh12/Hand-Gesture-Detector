5edebde45ddc81aa3cec1441e389e855cc65ab64
KareemMous <kareemmoustafa05@gmail.com>
2023-05-12 18:31:39 +0300

    add nn model

--- modules/models.py
+++ modules/models.py
@@ -1,7 +1,17 @@
+import tensorflow as tf
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn import svm
+import numpy as np
+
+
 def save_model(model, model_name):
     # save the model to disk
     filename = 'models/' + model_name + '.sav'
     pickle.dump(model, open(filename, 'wb'))
+
 
 def load_model(model_name):
     # load the model from disk
@@ -9,24 +19,20 @@ def load_model(model_name):
     loaded_model = pickle.load(open(filename, 'rb'))
     return loaded_model
 
-#This Module will have the models that we will use to train the dataset
-#1- SVM
-#2- Random Forest
-#3- KNN
-#4- Logistic Regression
-#5- Decision Tree
-#6- Naive Bayes
-#7- Neural Network
+# This Module will have the models that we will use to train the dataset
+# 1- SVM
+# 3- KNN
+# 4- Logistic Regression
+# 5- Decision Tree
+# 6- Naive Bayes
+# 7- Neural Network
 
-#SVM
-from sklearn import svm
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import classification_report
-from sklearn.model_selection import GridSearchCV
+
+# SVM
+
 
 def svm_model(train_features, train_labels, test_features, test_labels):
-    # Create the parameter grid based on the results of random search 
+    # Create the parameter grid based on the results of random search
     param_grid = {
         'C': [0.1, 1, 10, 100, 1000],
         'gamma': [1, 0.1, 0.01, 0.001, 0.0001],
@@ -35,8 +41,8 @@ def svm_model(train_features, train_labels, test_features, test_labels):
     # Create a based model
     svm_model = svm.SVC()
     # Instantiate the grid search model
-    grid_search = GridSearchCV(estimator = svm_model, param_grid = param_grid, 
-                              cv = 3, n_jobs = -1, verbose = 2)
+    grid_search = GridSearchCV(estimator=svm_model, param_grid=param_grid,
+                               cv=3, n_jobs=-1, verbose=2)
     # Fit the grid search to the data
     grid_search.fit(train_features, train_labels)
     best_grid = grid_search.best_estimator_
@@ -45,5 +51,37 @@ def svm_model(train_features, train_labels, test_features, test_labels):
     # Compute and print metrics
     print("Accuracy: {}".format(accuracy_score(test_labels, y_pred)))
     print("Confusion Matrix: {}".format(confusion_matrix(test_labels, y_pred)))
-    print("Classification Report: {}".format(classification_report(test_labels, y_pred)))
+    print("Classification Report: {}".format(
+        classification_report(test_labels, y_pred)))
     return best_grid
+
+
+def nn_model(train_features, train_labels, test_features, test_labels):
+
+    train_labels = [t[1] for t in train_labels]
+    test_labels = [t[1] for t in test_labels]
+    train_labels = [eval(t) for t in train_labels]
+    test_labels = [eval(t) for t in test_labels]
+
+    train_features = np.array(train_features)
+    train_labels = np.array(train_labels)
+    test_features = np.array(test_features)
+    test_labels = np.array(test_labels)
+
+    # Adding the input layer and the first hidden layer
+    input = tf.keras.Input(shape=(train_features.shape[1],))
+    x1 = tf.keras.layers.Dense(512, activation=tf.nn.relu)(input)
+    # Adding the second hidden layer
+    x2 = tf.keras.layers.Dense(256, activation=tf.nn.relu)(x1)
+    # Adding the output layer
+    output = tf.keras.layers.Dense(6, activation=tf.nn.softmax)(x2)
+    classifier = tf.keras.Model(inputs=input, outputs=output)
+    # Compiling the ANN
+    classifier.compile(
+        optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    # Fitting the ANN to the Training set
+    classifier.fit(train_features, train_labels, batch_size=32,
+                   epochs=100, validation_data=(test_features, test_labels))
+    # Predicting the Test set results
+
+    return classifier
