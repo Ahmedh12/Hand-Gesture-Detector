60ac154dcf93460a391b1c9de844c60e41ef9fdd
Ahmed Hussien <ahmedhussien783@gmail.com>
2023-05-13 05:18:08 +0300

    edit nn model function

--- modules/models.py
+++ modules/models.py
@@ -5,12 +5,14 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 from sklearn import svm
 import numpy as np
+import pickle
 
 
 def save_model(model, model_name):
     # save the model to disk
     filename = 'models/' + model_name + '.sav'
     pickle.dump(model, open(filename, 'wb'))
+    print("Model Saved Successfully")
 
 
 def load_model(model_name):
@@ -52,30 +54,24 @@ def svm_model(train_features, train_labels, test_features, test_labels):
     print("Accuracy: \n{}".format(accuracy_score(test_labels, y_pred)))
     print("Confusion Matrix: \n{}".format(confusion_matrix(test_labels, y_pred)))
     print("Classification Report: \n{}".format(classification_report(test_labels, y_pred)))
-    print("Accuracy: {}".format(accuracy_score(test_labels, y_pred)))
-    print("Confusion Matrix: {}".format(confusion_matrix(test_labels, y_pred)))
-    print("Classification Report: {}".format(
-        classification_report(test_labels, y_pred)))
     return best_grid
 
 
-def nn_model(train_features, train_labels, test_features, test_labels):
-
-
+def nn_model(train_features, train_labels, test_features, test_labels , epochs=10):
     # Adding the input layer and the first hidden layer
-    input = tf.keras.Input(shape=(train_features.shape[1],))
-    x1 = tf.keras.layers.Dense(512, activation=tf.nn.relu)(input)
+    input_size = train_features.shape[1]
+    input = tf.keras.Input(shape=(input_size,))
+    x1 = tf.keras.layers.Dense(input_size, activation=tf.nn.relu)(input)
     # Adding the second hidden layer
-    x2 = tf.keras.layers.Dense(256, activation=tf.nn.relu)(x1)
+    x2 = tf.keras.layers.Dense(input_size//2, activation=tf.nn.relu)(x1)
     # Adding the output layer
     output = tf.keras.layers.Dense(6, activation=tf.nn.softmax)(x2)
     classifier = tf.keras.Model(inputs=input, outputs=output)
     # Compiling the ANN
-    classifier.compile(
-        optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    classifier.compile(optimizer = 'adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
     # Fitting the ANN to the Training set
-    classifier.fit(train_features, train_labels, batch_size=32,
-                   epochs=100, validation_data=(test_features, test_labels))
-    # Predicting the Test set results
+    classifier.fit(train_features, train_labels, batch_size=2,
+                   epochs=epochs, validation_data=(test_features, test_labels))
+    
 
     return classifier
